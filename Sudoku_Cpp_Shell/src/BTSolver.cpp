ff15e66d2b2ae1792348e2fefa5565b272c550dd
Joseph Nguyen <josnguyen.kl@gmail.com>
2018-02-13 11:47:15 -0800

    Adding shell - newest version 2/13

---
+++ Sudoku_Cpp_Shell/src/BTSolver.cpp
@@ -0,0 +1,285 @@
+#include"BTSolver.hpp"
+
+using namespace std;
+
+// =====================================================================
+// Constructors
+// =====================================================================
+
+BTSolver::BTSolver ( SudokuBoard input, Trail* _trail,  string val_sh, string var_sh, string cc )
+: sudokuGrid( input.get_p(), input.get_q(), input.get_board() ), network( input )
+{
+	valHeuristics = val_sh;
+	varHeuristics = var_sh;
+	cChecks =  cc;
+
+	trail = _trail;
+}
+
+// =====================================================================
+// Consistency Checks
+// =====================================================================
+
+// Basic consistency check, no propagation done
+bool BTSolver::assignmentsCheck ( void )
+{
+	for ( Constraint c : network.getConstraints() )
+		if ( ! c.isConsistent() )
+			return false;
+
+	return true;
+}
+
+/**
+ * Part 1 TODO: Implement the Forward Checking Heuristic
+ *
+ * This function will do both Constraint Propagation and check
+ * the consistency of the network
+ *
+ * (1) If a variable is assigned then eliminate that value from
+ *     the square's neighbors.
+ *
+ * Note: remember to trail.push variables before you assign them
+ * Return: true is assignment is consistent, false otherwise
+ */
+bool BTSolver::forwardChecking ( void )
+{
+	return false;
+}
+
+/**
+ * Part 2 TODO: Implement both of Norvig's Heuristics
+ *
+ * This function will do both Constraint Propagation and check
+ * the consistency of the network
+ *
+ * (1) If a variable is assigned then eliminate that value from
+ *     the square's neighbors.
+ *
+ * (2) If a constraint has only one possible place for a value
+ *     then put the value there.
+ *
+ * Note: remember to trail.push variables before you assign them
+ * Return: true is assignment is consistent, false otherwise
+ */
+bool BTSolver::norvigCheck ( void )
+{
+	return false;
+}
+
+/**
+ * Optional TODO: Implement your own advanced Constraint Propagation
+ *
+ * Completing the three tourn heuristic will automatically enter
+ * your program into a tournament.
+ */
+bool BTSolver::getTournCC ( void )
+{
+	return false;
+}
+
+// =====================================================================
+// Variable Selectors
+// =====================================================================
+
+// Basic variable selector, returns first unassigned variable
+Variable* BTSolver::getfirstUnassignedVariable ( void )
+{
+	for ( Variable* v : network.getVariables() )
+		if ( !(v->isAssigned()) )
+			return v;
+
+	// Everything is assigned
+	return nullptr;
+}
+
+/**
+ * Part 1 TODO: Implement the Minimum Remaining Value Heuristic
+ *
+ * Return: The unassigned variable with the smallest domain
+ */
+Variable* BTSolver::getMRV ( void )
+{
+	return nullptr;
+}
+
+/**
+ * Part 2 TODO: Implement the Minimum Remaining Value Heuristic
+ *                with Degree Heuristic as a Tie Breaker
+ *
+ * Return: The unassigned variable with, first, the smallest domain
+ *         and, second, the most unassigned neighbors
+ */
+Variable* BTSolver::getDegree ( void )
+{
+	return nullptr;
+}
+
+/**
+ * Part 2 TODO: Implement the Minimum Remaining Value Heuristic
+ *                with Degree Heuristic as a Tie Breaker
+ *
+ * Return: The unassigned variable with the smallest domain and involved
+ *             in the most constraints
+ */
+Variable* BTSolver::MRVwithTieBreaker ( void )
+{
+	return nullptr;
+}
+
+/**
+ * Optional TODO: Implement your own advanced Variable Heuristic
+ *
+ * Completing the three tourn heuristic will automatically enter
+ * your program into a tournament.
+ */
+Variable* BTSolver::getTournVar ( void )
+{
+	return nullptr;
+}
+
+// =====================================================================
+// Value Selectors
+// =====================================================================
+
+// Default Value Ordering
+vector<int> BTSolver::getValuesInOrder ( Variable* v )
+{
+	vector<int> values = v->getDomain().getValues();
+	sort( values.begin(), values.end() );
+	return values;
+}
+
+/**
+ * Part 1 TODO: Implement the Least Constraining Value Heuristic
+ *
+ * The Least constraining value is the one that will knock the least
+ * values out of it's neighbors domain.
+ *
+ * Return: A list of v's domain sorted by the LCV heuristic
+ *         The LCV is first and the MCV is last
+ */
+vector<int> BTSolver::getValuesLCVOrder ( Variable* v )
+{
+	return vector<int>();
+}
+
+/**
+ * Optional TODO: Implement your own advanced Value Heuristic
+ *
+ * Completing the three tourn heuristic will automatically enter
+ * your program into a tournament.
+ */
+vector<int> BTSolver::getTournVal ( Variable* v )
+{
+	return vector<int>();
+}
+
+// =====================================================================
+// Engine Functions
+// =====================================================================
+
+void BTSolver::solve ( void )
+{
+	if ( hasSolution )
+		return;
+
+	// Variable Selection
+	Variable* v = selectNextVariable();
+
+	if ( v == nullptr )
+	{
+		for ( Variable* var : network.getVariables() )
+		{
+			// If all variables haven't been assigned
+			if ( ! ( var->isAssigned() ) )
+			{
+				cout << "Error" << endl;
+				return;
+			}
+		}
+
+		// Success
+		hasSolution = true;
+		return;
+	}
+
+	// Attempt to assign a value
+	for ( int i : getNextValues( v ) )
+	{
+		// Store place in trail and push variable's state on trail
+		trail->placeTrailMarker();
+		trail->push( v );
+
+		// Assign the value
+		v->assignValue( i );
+
+		// Propagate constraints, check consistency, recurse
+		if ( checkConsistency() )
+			solve();
+
+		// If this assignment succeeded, return
+		if ( hasSolution )
+			return;
+
+		// Otherwise backtrack
+		trail->undo();
+	}
+}
+
+bool BTSolver::checkConsistency ( void )
+{
+	if ( cChecks == "forwardChecking" )
+		return forwardChecking();
+
+	if ( cChecks == "norvigCheck" )
+		return norvigCheck();
+
+	if ( cChecks == "tournCC" )
+		return getTournCC();
+
+	return assignmentsCheck();
+}
+
+Variable* BTSolver::selectNextVariable ( void )
+{
+	if ( varHeuristics == "MinimumRemainingValue" )
+		return getMRV();
+
+	if ( varHeuristics == "Degree" )
+		return getDegree();
+
+	if ( varHeuristics == "MRVwithTieBreaker" )
+		return MRVwithTieBreaker();
+
+	if ( varHeuristics == "tournVar" )
+		return getTournVar();
+
+	return getfirstUnassignedVariable();
+}
+
+vector<int> BTSolver::getNextValues ( Variable* v )
+{
+	if ( valHeuristics == "LeastConstrainingValue" )
+		return getValuesLCVOrder( v );
+
+	if ( valHeuristics == "tournVal" )
+		return getTournVal( v );
+
+	return getValuesInOrder( v );
+}
+
+bool BTSolver::haveSolution ( void )
+{
+	return hasSolution;
+}
+
+SudokuBoard BTSolver::getSolution ( void )
+{
+	return network.toSudokuBoard ( sudokuGrid.get_p(), sudokuGrid.get_q() );
+}
+
+ConstraintNetwork BTSolver::getNetwork ( void )
+{
+	return network;
+}
---
+++ Sudoku_Java_Shell/src/BTSolver.java
@@ -0,0 +1,322 @@
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class BTSolver
+{
+
+	// =================================================================
+	// Properties
+	// =================================================================
+
+	private ConstraintNetwork network;
+	private SudokuBoard sudokuGrid;
+	private Trail trail;
+
+	private boolean hasSolution = false;
+
+	public String varHeuristics;
+	public String valHeuristics;
+	public String cChecks;
+
+	// =================================================================
+	// Constructors
+	// =================================================================
+
+	public BTSolver ( SudokuBoard sboard, Trail trail, String val_sh, String var_sh, String cc )
+	{
+		this.network    = new ConstraintNetwork( sboard );
+		this.sudokuGrid = sboard;
+		this.trail      = trail;
+
+		varHeuristics = var_sh;
+		valHeuristics = val_sh;
+		cChecks       = cc;
+	}
+
+	// =================================================================
+	// Consistency Checks
+	// =================================================================
+
+	// Basic consistency check, no propagation done
+	private boolean assignmentsCheck ( )
+	{
+		for ( Constraint c : network.getConstraints() )
+			if ( ! c.isConsistent() )
+				return false;
+
+		return true;
+	}
+
+	/**
+	 * Part 1 TODO: Implement the Forward Checking Heuristic
+	 *
+	 * This function will do both Constraint Propagation and check
+	 * the consistency of the network
+	 *
+	 * (1) If a variable is assigned then eliminate that value from
+	 *     the square's neighbors.
+	 *
+	 * Note: remember to trail.push variables before you assign them
+	 * Return: true is assignment is consistent, false otherwise
+	 */
+	private boolean forwardChecking ( )
+	{
+		return false;
+	}
+
+	/**
+	 * Part 2 TODO: Implement both of Norvig's Heuristics
+	 *
+	 * This function will do both Constraint Propagation and check
+	 * the consistency of the network
+	 *
+	 * (1) If a variable is assigned then eliminate that value from
+	 *     the square's neighbors.
+	 *
+	 * (2) If a constraint has only one possible place for a value
+	 *     then put the value there.
+	 *
+	 * Note: remember to trail.push variables before you assign them
+	 * Return: true is assignment is consistent, false otherwise
+	 */
+	private boolean norvigCheck ( )
+	{
+		return false;
+	}
+
+	/**
+	 * Optional TODO: Implement your own advanced Constraint Propagation
+	 *
+	 * Completing the three tourn heuristic will automatically enter
+	 * your program into a tournament.
+	 */
+	private boolean getTournCC ( )
+	{
+		return false;
+	}
+
+	// =================================================================
+	// Variable Selectors
+	// =================================================================
+
+	// Basic variable selector, returns first unassigned variable
+	private Variable getfirstUnassignedVariable()
+	{
+		for ( Variable v : network.getVariables() )
+			if ( ! v.isAssigned() )
+				return v;
+
+		// Everything is assigned
+		return null;
+	}
+
+	/**
+	 * Part 1 TODO: Implement the Minimum Remaining Value Heuristic
+	 *
+	 * Return: The unassigned variable with the smallest domain
+	 */
+	private Variable getMRV ( )
+	{
+		return null;
+	}
+
+	/**
+	 * Part 2 TODO: Implement the Degree Heuristic
+	 *
+	 * Return: The unassigned variable with the most unassigned neighbors
+	 */
+	private Variable getDegree ( )
+	{
+		return null;
+	}
+
+	/**
+	 * Part 2 TODO: Implement the Minimum Remaining Value Heuristic
+	 *                with Degree Heuristic as a Tie Breaker
+	 *
+	 * Return: The unassigned variable with, first, the smallest domain
+	 *         and, second, the most unassigned neighbors
+	 */
+	private Variable MRVwithTieBreaker ( )
+	{
+		return null;
+	}
+
+	/**
+	 * Optional TODO: Implement your own advanced Variable Heuristic
+	 *
+	 * Completing the three tourn heuristic will automatically enter
+	 * your program into a tournament.
+	 */
+	private Variable getTournVar ( )
+	{
+		return null;
+	}
+
+	// =================================================================
+	// Value Selectors
+	// =================================================================
+
+	// Default Value Ordering
+	public List<Integer> getValuesInOrder ( Variable v )
+	{
+		List<Integer> values = v.getDomain().getValues();
+
+		Comparator<Integer> valueComparator = new Comparator<Integer>(){
+
+			@Override
+			public int compare(Integer i1, Integer i2) {
+				return i1.compareTo(i2);
+			}
+		};
+		Collections.sort(values, valueComparator);
+		return values;
+	}
+
+	/**
+	 * Part 1 TODO: Implement the Least Constraining Value Heuristic
+	 *
+	 * The Least constraining value is the one that will knock the least
+	 * values out of it's neighbors domain.
+	 *
+	 * Return: A list of v's domain sorted by the LCV heuristic
+	 *         The LCV is first and the MCV is last
+	 */
+	public List<Integer> getValuesLCVOrder ( Variable v )
+	{
+		return null;
+	}
+
+	/**
+	 * Optional TODO: Implement your own advanced Value Heuristic
+	 *
+	 * Completing the three tourn heuristic will automatically enter
+	 * your program into a tournament.
+	 */
+	public List<Integer> getTournVal ( Variable v )
+	{
+		return null;
+	}
+
+	//==================================================================
+	// Engine Functions
+	//==================================================================
+
+	public void solve ( )
+	{
+		if ( hasSolution )
+			return;
+
+		// Variable Selection
+		Variable v = selectNextVariable();
+
+		if ( v == null )
+		{
+			for ( Variable var : network.getVariables() )
+			{
+				// If all variables haven't been assigned
+				if ( ! var.isAssigned() )
+				{
+					System.out.println( "Error" );
+					return;
+				}
+			}
+
+			// Success
+			hasSolution = true;
+			return;
+		}
+
+		// Attempt to assign a value
+		for ( Integer i : getNextValues( v ) )
+		{
+			// Store place in trail and push variable's state on trail
+			trail.placeTrailMarker();
+			trail.push( v );
+
+			// Assign the value
+			v.assignValue( i );
+
+			// Propagate constraints, check consistency, recurse
+			if ( checkConsistency() )
+				solve();
+
+			// If this assignment succeeded, return
+			if ( hasSolution )
+				return;
+
+			// Otherwise backtrack
+			trail.undo();
+		}
+	}
+
+	private boolean checkConsistency ( )
+	{
+		switch ( cChecks )
+		{
+			case "forwardChecking":
+				return forwardChecking();
+
+			case "norvigCheck":
+				return norvigCheck();
+
+			case "tournCC":
+				return getTournCC();
+
+			default:
+				return assignmentsCheck();
+		}
+	}
+
+	private Variable selectNextVariable ( )
+	{
+		switch ( varHeuristics )
+		{
+			case "MinimumRemainingValue":
+				return getMRV();
+
+			case "Degree":
+				return getDegree();
+
+			case "MRVwithTieBreaker":
+				return MRVwithTieBreaker();
+
+			case "tournVar":
+				return getTournVar();
+
+			default:
+				return getfirstUnassignedVariable();
+		}
+	}
+
+	public List<Integer> getNextValues ( Variable v )
+	{
+		switch ( valHeuristics )
+		{
+			case "LeastConstrainingValue":
+				return getValuesLCVOrder( v );
+
+			case "tournVal":
+				return getTournVal( v );
+
+			default:
+				return getValuesInOrder( v );
+		}
+	}
+
+	public boolean hasSolution ( )
+	{
+		return hasSolution;
+	}
+
+	public SudokuBoard getSolution ( )
+	{
+		return network.toSudokuBoard ( sudokuGrid.getP(), sudokuGrid.getQ() );
+	}
+
+	public ConstraintNetwork getNetwork ( )
+	{
+		return network;
+	}
+}
